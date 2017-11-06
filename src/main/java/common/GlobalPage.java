92d6bbb9e4c48aabfdb8e39073d77d39a1735e7d
Mavruk <212617361@logon.ds.ge.com>
2017-11-06 13:46:04 +0300

    US137574_NewApphubNavbar

--- src/main/java/common/GlobalPage.java
+++ src/main/java/common/GlobalPage.java
@@ -10,6 +10,9 @@ public class GlobalPage extends PageBase
 {
     public static NavigationSection navigationSection = new NavigationSection();
     public static MainNavigationOptions mainNavigationOptions = new MainNavigationOptions();
+    public static MainPXNavigationOptions mainPXNavigationOptions = new MainPXNavigationOptions();
+    public static TimeUTC timeUTC = new TimeUTC();
+
     public static MainHeader        mainHeader        = new MainHeader();
 
     public static class NavigationSection extends Element
@@ -56,7 +59,58 @@ public class GlobalPage extends PageBase
                 icon = new Element(this.name + " / Icon", By.xpath(".//i[contains(@class,'item-icon')]"), this);
                 navText = new Element(this.name + " Text", By.xpath(".//div[contains(@class, 'item-text')]"), this);
             }
+
         }
+    }
+
+    public static class MainPXNavigationOptions extends Element {
+
+       // public final Element configPageButton;
+
+        public MainPXNavigationOptions()
+        {
+            super("Main Navigation Options", By.xpath("//*[@id='items']"));
+        //    configPageButton = new Element("Pax Config Button", By.xpath(".//p[contains(text(),'Pax Protection')]"), this);
+        }
+
+        public NavigateTo navigateToNavbarLink(String name)
+        {
+            return new MainPXNavigationOptions.NavigateTo("Navigate To: " + name, By.xpath(".//p[contains(text(),'" + name + "')]"), this);
+        }
+
+        public static class NavigateTo extends Element
+        {
+            public final Element icon;
+            public final Element navText;
+            public final Element impactPage;
+            public final Element protectionPage;
+
+
+            public NavigateTo(
+                    String name,
+                    By by,
+                    Element parent)
+            {
+                super(name, by, parent);
+                icon = new Element(this.name + " / Icon", By.xpath("//*[@id='items']//px-icon[contains(@class,'px-app-nav-item') and @icon='fa:cog']"), this);
+                navText = new Element(this.name + " Text", By.xpath(".//p[contains(text(),'" + name + "']"), this);
+                impactPage = new Element(this.name + " Text", By.xpath("/html/body/app-hub-shell/poly-app-app/iron-pages/poly-disruptions/div"), this);
+                protectionPage = new Element(this.name + " Text", By.xpath("/html/body/app-hub-shell/poly-app-app/iron-pages/poly-config/div"), this);
+                // impactPage = new Element(this.name + " Text", By.xpath(".//poly-disruptions[@name='disruptions']//div[contains(@class,'poly-disruptions')]"), this);
+               // protectionPage = new Element(this.name + " Text", By.xpath(".//poly-config[@name='config']/div"), this);
+            }
+        }
+
+    }
+
+    public static class TimeUTC extends Element
+    {
+       public final Element timeUTCText;
+
+       public TimeUTC(){
+           super("Time shown in UTC Label",By.xpath("/html/body/app-hub-shell/div/nav-bar/div/div"));
+           timeUTCText = new Element("Time shown in UTC Text",By.xpath(".//span[contains(text(),'Time shown in UTC')]"),this);
+       }
     }
 
     public static class MainHeader extends Element
---
+++ src/test/java/Sprint22/US137574_NewApphubNavbar.java
@@ -0,0 +1,85 @@
+package Sprint22;
+
+import org.testng.annotations.Test;
+import auto.framework.ReportLog;
+import auto.framework.TestBase;
+import auto.framework.web.WebControl;
+import common.GlobalPage;
+import common.TestDataHandler;
+import pageobjects.GESSOAuthPage;
+import pageobjects.ConfigPage;
+
+public class US137574_NewApphubNavbar extends TestBase{
+    public static TestDataHandler testDataHandler;
+    @Test
+    public void TestScenarios() throws Exception
+    {
+        ReportLog.setTestName("Test");
+        testDataHandler = TestDataHandler.loadTestData("URL", "RowSelection='LoginUser1'");
+        TestCases.PreRequisiteStep(testDataHandler);
+        TestCases.Step1();
+        TestCases.Step2();
+        TestCases.Step3();
+        TestCases.Step4();
+    }
+
+    public static class TestCases {
+        public static void PreRequisiteStep(TestDataHandler testDataHandler) throws InterruptedException {
+            ReportLog.setTestCase("PAX Protection HomePage");
+            ReportLog.setTestStep("Go to this URL: https://pax-protect-ui-shell.run.aws-usw02-pr.ice.predix.io ");
+            WebControl.clearData();
+            WebControl.open(testDataHandler.url);
+
+            Thread.sleep(1000);
+            if (GESSOAuthPage.authInfo.gESSOLabel.isDisplayed()) {
+                GESSOAuthPage.authInfo.sSOIDInput.typeKeys(testDataHandler.username);
+                GESSOAuthPage.authInfo.passWordInput.typeKeys(testDataHandler.password);
+                GESSOAuthPage.authInfo.submitFormShared.click();
+            }
+            GESSOAuthPage.page.verifyURL(false, 60);
+        }
+
+        public static void Step1() {
+            ReportLog.setTestCase("[STEP 1]");
+            ReportLog.setTestStep("Verify Pax Protection Text and Icon");
+
+            GlobalPage.mainPXNavigationOptions.navigateToNavbarLink("Pax Protection").highlight();
+            GlobalPage.mainPXNavigationOptions.navigateToNavbarLink("Pax Protection").verifyDisplayed(true, 5);
+            GlobalPage.mainPXNavigationOptions.navigateToNavbarLink("Pax Protection").icon.highlight();
+            GlobalPage.mainPXNavigationOptions.navigateToNavbarLink("Pax Protection").verifyText("Pax Protection");
+            GlobalPage.mainPXNavigationOptions.navigateToNavbarLink("Pax Impact").highlight();
+            GlobalPage.mainPXNavigationOptions.navigateToNavbarLink("Pax Impact").verifyDisplayed(true, 5);
+            GlobalPage.mainPXNavigationOptions.navigateToNavbarLink("Pax Impact").verifyText("Pax Impact");
+
+
+        }
+
+        public static void Step2() {
+
+            ReportLog.setTestCase("[STEP 2]");
+            ReportLog.setTestStep("Verify Pax Impact Page");
+            GlobalPage.mainPXNavigationOptions.navigateToNavbarLink("Pax Impact").click();
+            GlobalPage.mainPXNavigationOptions.navigateToNavbarLink("Pax Impact").impactPage.verifyDisplayed(true, 5);
+
+        }
+
+        public static void Step3() {
+
+            ReportLog.setTestCase("[STEP 3]");
+            ReportLog.setTestStep("Verify Pax Protection Page");
+            GlobalPage.mainPXNavigationOptions.navigateToNavbarLink("Pax Protection").click();
+            GlobalPage.mainPXNavigationOptions.navigateToNavbarLink("Pax Protection").protectionPage.verifyDisplayed(true, 5);
+        }
+
+        public static void Step4() {
+
+            ReportLog.setTestCase("[STEP 4]");
+            ReportLog.setTestStep("Verify Time shown in UTC Text");
+            GlobalPage.timeUTC.timeUTCText.highlight();
+            GlobalPage.timeUTC.timeUTCText.verifyText("Time shown in UTC");
+
+        }
+
+    }
+
+}
