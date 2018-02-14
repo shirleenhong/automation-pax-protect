c0d7b4e44f8b3ceacc730aed8b9781ed15b20c1e
shirleen.c.hong <shirleen.c.hong@GEAP-517206M-LX.dir.svc.accenture.com>
2018-02-14 08:26:40 +0800

    02/14/2018 CHANGES: Page Objects: - PaxImpactpage.java > Filter - TestDataHandler.java
    Test Page:
    - Sprint3_18Q1 >  US151900_SearchingInPDS.java
    
    - PaxProtect\src\test\resources\data\Dataset
    
    Change-Id: I8c33184c692803861c9817abab9f5c6a4c21416b

--- src/main/java/pageobjects/PDSListViewPage.java
+++ src/main/java/pageobjects/PDSListViewPage.java
@@ -25,11 +25,13 @@ public class PDSListViewPage extends GlobalPage
 		public static class DisruptedItem extends Element
 		{
 			public final Element listViewCheckBox;
+			public final Element listViewFlightNumber;
 			
 			public DisruptedItem (String id)
 			{
 				super("List View - Flight ID: " + id, By.xpath("//*[contains(@class,'list style-scope ppro-list')]//*[contains(@id,'"+id+"')]"));
 				listViewCheckBox = new Element("List View Check Box " + id, By.xpath(".//div[@class='squarebox style-scope ppro-checkbox']"), this);
+				listViewFlightNumber = new Element("Impacted Flights Text", By.xpath(".//div[@class='ppro-table-cell style-scope ppro-flight-line']"));
 
 			}
 		}
