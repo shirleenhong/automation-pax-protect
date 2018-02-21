6d6c50ca73f65ef342e6de8e9f4df505dbfb7a1f
212617361 <pinar.mavruk@ge.com>
2018-02-21 21:35:04 +0300

    otp service get request for arr is added

--- src/test/java/Sprint4_18Q1/US157178_Implement_px_kpi_with_two_lines.java
+++ src/test/java/Sprint4_18Q1/US157178_Implement_px_kpi_with_two_lines.java
@@ -3,17 +3,29 @@ package Sprint4_18Q1;
 import auto.framework.ReportLog;
 import auto.framework.TestBase;
 import auto.framework.web.WebControl;
+import common.BackendAPI;
 import common.GlobalPage;
 import common.TestDataHandler;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 import pageobjects.ExecutiveDashboardPage;
 import pageobjects.GESSOAuthPage;
 
 import java.net.MalformedURLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class US157178_Implement_px_kpi_with_two_lines extends TestBase {
 
     public static TestDataHandler testDataHandler;
+
+    public static BackendAPI backendAPI = new BackendAPI();
+
+    public static String responseContent;
 
     @Test
     public void TestScenarios() throws Exception
@@ -22,6 +34,11 @@ public class US157178_Implement_px_kpi_with_two_lines extends TestBase {
         testDataHandler = TestDataHandler.loadTestData("URL", "RowSelection='LoginUser1'");
         US157178_Implement_px_kpi_with_two_lines.TestCases.PreRequisiteStep();
         US157178_Implement_px_kpi_with_two_lines.TestCases.Step1();
+        US157178_Implement_px_kpi_with_two_lines.TestCases.Step2();
+        US157178_Implement_px_kpi_with_two_lines.TestCases.Step3();
+        US157178_Implement_px_kpi_with_two_lines.TestCases.Step4();
+        US157178_Implement_px_kpi_with_two_lines.TestCases.Step5();
+        US157178_Implement_px_kpi_with_two_lines.TestCases.Step6();
 
     }
 
@@ -57,10 +74,37 @@ public class US157178_Implement_px_kpi_with_two_lines extends TestBase {
             ExecutiveDashboardPage.statisticFrame.statisticItem("ARRIVALS OTP").kpiChartCount.verifyDisplayed();
             ExecutiveDashboardPage.statisticFrame.statisticItem("ARRIVALS OTP").kpiChartCountText.verifyDisplayed();
 
-            String count = ExecutiveDashboardPage.statisticFrame.statisticItem("ARRIVALS OTP").kpiChartCount.getAttribute("count");
-            String countText = ExecutiveDashboardPage.statisticFrame.statisticItem("ARRIVALS OTP").kpiChartCountText.getText();
+            String arrOtpUiCount = ExecutiveDashboardPage.statisticFrame.statisticItem("ARRIVALS OTP").kpiChartCount.getAttribute("count");
+            String arrOtpUiCountText = ExecutiveDashboardPage.statisticFrame.statisticItem("ARRIVALS OTP").kpiChartCountText.getText();
 
             String d = ExecutiveDashboardPage.statisticFrame.statisticItem("ARRIVALS OTP").rectPathItem.getAttribute("d");
+
+            SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd");//dd/MM/yyyy
+            //Date now = new Date();
+            String strDate = sdfDate.format(new Date());
+            // parse date from yyyy-mm-dd pattern
+            LocalDate today = LocalDate.parse(strDate);
+            // add one day
+            LocalDate nextDay = today.plusDays(1);
+
+            String q = "{\"destAirportID\":\"AP-LAX\",\"arrDateTimeSch\":{\"$and\":[{\"$ge\":{\"$date\":\"" + today + "T00:01:00Z\"}},{\"$lt\":{\"$date\":\"" + nextDay + "T00:01:00Z\"}}]}}";
+
+            responseContent = backendAPI.getPayloadWithProperty("Positive Test", "GET/OTP","request_GET_otp_ARR", "Request", q);
+
+            JSONArray jsonArrayS = new JSONArray(responseContent);
+            int otpArrCount = jsonArrayS.length();
+
+            if ((Integer.parseInt(arrOtpUiCount) == otpArrCount) && (Integer.parseInt(arrOtpUiCountText) == otpArrCount)) {
+                ReportLog.assertTrue(true, "Arrival OTP test passed");
+            }else{
+                ReportLog.assertFailed("Arrival OTP test failed");
+            }
+
+
+
+        }
+
+        public static void Step2(){
 
             ExecutiveDashboardPage.statisticFrame.statisticItem("CANCELLED").verifyDisplayed(true,5);
             ExecutiveDashboardPage.statisticFrame.statisticItem("CANCELLED").statisticItemHeader.verifyDisplayed(true,5);
@@ -68,24 +112,42 @@ public class US157178_Implement_px_kpi_with_two_lines extends TestBase {
             ExecutiveDashboardPage.statisticFrame.statisticItem("CANCELLED").rectPathItem.verifyDisplayed(true,5);
             ExecutiveDashboardPage.statisticFrame.statisticItem("CANCELLED").kpiChartCount.verifyDisplayed();
             ExecutiveDashboardPage.statisticFrame.statisticItem("CANCELLED").kpiChartCountText.verifyDisplayed();
+
+        }
+
+        public static void Step3(){
             ExecutiveDashboardPage.statisticFrame.statisticItem("MISCONX").verifyDisplayed(true,5);
             ExecutiveDashboardPage.statisticFrame.statisticItem("MISCONX").statisticItemHeader.verifyDisplayed(true,5);
             ExecutiveDashboardPage.statisticFrame.statisticItem("MISCONX").rectItem.verifyDisplayed(true,5);
             ExecutiveDashboardPage.statisticFrame.statisticItem("MISCONX").rectPathItem.verifyDisplayed(true,5);
             ExecutiveDashboardPage.statisticFrame.statisticItem("MISCONX").kpiChartCount.verifyDisplayed();
             ExecutiveDashboardPage.statisticFrame.statisticItem("MISCONX").kpiChartCountText.verifyDisplayed();
+
+        }
+
+        public static void Step4(){
             ExecutiveDashboardPage.statisticFrame.statisticItem("DEPARTURES OTP").verifyDisplayed(true,5);
             ExecutiveDashboardPage.statisticFrame.statisticItem("DEPARTURES OTP").statisticItemHeader.verifyDisplayed(true,5);
             ExecutiveDashboardPage.statisticFrame.statisticItem("DEPARTURES OTP").rectItem.verifyDisplayed(true,5);
             ExecutiveDashboardPage.statisticFrame.statisticItem("DEPARTURES OTP").rectPathItem.verifyDisplayed(true,5);
             ExecutiveDashboardPage.statisticFrame.statisticItem("DEPARTURES OTP").kpiChartCount.verifyDisplayed();
             ExecutiveDashboardPage.statisticFrame.statisticItem("DEPARTURES OTP").kpiChartCountText.verifyDisplayed();
+
+        }
+
+        public static void Step5(){
             ExecutiveDashboardPage.statisticFrame.statisticItem("DIVERTED").verifyDisplayed(true,5);
             ExecutiveDashboardPage.statisticFrame.statisticItem("DIVERTED").statisticItemHeader.verifyDisplayed(true,5);
             ExecutiveDashboardPage.statisticFrame.statisticItem("DIVERTED").rectItem.verifyDisplayed(true,5);
             ExecutiveDashboardPage.statisticFrame.statisticItem("DIVERTED").rectPathItem.verifyDisplayed(true,5);
             ExecutiveDashboardPage.statisticFrame.statisticItem("DIVERTED").kpiChartCount.verifyDisplayed();
             ExecutiveDashboardPage.statisticFrame.statisticItem("DIVERTED").kpiChartCountText.verifyDisplayed();
+
+
+        }
+
+        public static void Step6(){
+
             ExecutiveDashboardPage.statisticFrame.statisticItem("> 180min").verifyDisplayed(true,5);
             ExecutiveDashboardPage.statisticFrame.statisticItem("> 180min").statisticItemHeader.verifyDisplayed(true,5);
             ExecutiveDashboardPage.statisticFrame.statisticItem("> 180min").rectItem.verifyDisplayed(true,5);
